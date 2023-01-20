498f03906b8d8320e92ded016e4b4068253f0b53
Jort Kuhlmann <jortkuhlmann1@gmail.com>
2023-01-20 19:19:07 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,25 @@ import { ThemeProvider } from "@mayntri/frontend-core";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Routes } from "./routes";
 import { NavigationScroll } from "./layouts/NavigationScroll";
 
 const queryClient = new QueryClient();
 
-const isHostedOnGithubPages = location.host === "mayntri.github.io";
+const isHostedOnGithubPages = location.host !== "mayntri.github.io";
+
+const RouterType = ({ children }: { children: React.ReactNode }) => {
+  return isHostedOnGithubPages ? (
+    <HashRouter>{children}</HashRouter>
+  ) : (
+    <BrowserRouter>{children}</BrowserRouter>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={isHostedOnGithubPages ? "/vite-project" : ""}>
+    <RouterType>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider>
           <NavigationScroll>
@@ -20,6 +28,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
           </NavigationScroll>
         </ThemeProvider>
       </QueryClientProvider>
-    </BrowserRouter>
+    </RouterType>
   </React.StrictMode>
 );
