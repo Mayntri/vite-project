1cccb2778f339001199e214c6242881465e4b60f
Jort Kuhlmann <jortkuhlmann1@gmail.com>
2023-01-20 19:22:09 +0100

    Updates

---
+++ assets/DashboardUtils-55e2808f.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-6ce8e47d.js";const r=()=>s.jsx("div",{className:"App",children:"test"});export{r as default};
--- assets/DashboardUtils-ad15c830.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-3e84dac7.js";const r=()=>s.jsx("div",{className:"App",children:"test"});export{r as default};
